e0784f7adc36c6e51a82202ac0bdd39668dc27c2
FrankieChik <59936546+FrankieChik@users.noreply.github.com>
2020-04-20 23:12:42 -0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from pandas.plotting import andrews_curves
 
 names = ['Topic', 'Weight', 'Words']
 
 list(open('txt2001_topickeys.txt'))
-result1 = pd.read_table('txt2001_topickeys.txt', names=names)
-print(result1)
+df = pd.read_table('txt2001_topickeys.txt', names=names)
 
-list(open('txt2008_topickeys.txt'))
-result2 = pd.read_table('txt2008_topickeys.txt', names=names)
-print(result2)
+del df['Words']
+print(df)
+
+df.plot.bar(x='Topic', y='Weight' )
+plt.show()
 
 
 
