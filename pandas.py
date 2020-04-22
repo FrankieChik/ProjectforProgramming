848e204b2000edf3fea9f1ede907a021a92094e3
FrankieChik <59936546+FrankieChik@users.noreply.github.com>
2020-04-22 13:16:42 -0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot as plt
 
 names = ['Topic', 'Weight', 'Words']
 
-list(open('txt2001_topickeys.txt'))
-df = pd.read_table('txt2001_topickeys.txt', names=names)
+# I create a function that allows me to transfer the results of each year into pandas. After that, the results shown in pandads will be visualized in mutiple bar charts.
+def read_table (txt_files):
+    list(open(txt_files))
+    df = pd.read_table(txt_files, names=names)
+    del df['Words']
+    print(df)
+    df.plot.bar(x='Topic', y='Weight')
+    plt.show()
+    return
 
-del df['Words']
-print(df)
+df1 = read_table ('txt2001_topickeys.txt')
+print (df1)
 
-df.plot.bar(x='Topic', y='Weight' )
-plt.show()
-
-
-
+df2= read_table ('txt2008_topickeys.txt')
+print(df2)
