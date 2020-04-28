191ba339721bc504288090528fca6aaaa9d03550
FrankieChik <59936546+FrankieChik@users.noreply.github.com>
2020-04-28 16:02:19 -0700

    Update Bibliography.py

--- Bibliography.py
+++ Bibliography.py
@@ -2,32 +2,41 @@
 # First, to create a list to show all the txt files I will analyze in the following sections.
 # Second, I what to figure out 10 major topics my data set includes.
 # Third, to visualize my topic modeling results.
+# Extra work: To network the topics and my documents.
 
 import os
 import gensim, nltk
 from nltk.corpus import stopwords
+from nltk.corpus import PlaintextCorpusReader
 from stop_words import get_stop_words
+from nltk.tokenize import word_tokenize
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from wordcloud import WordCloud, STOPWORDS
 from os import walk
 import numpy as ny
 import csv
+from pylab import *
+from collections import Counter
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+
 
 # The first task I want to do, is to create a list in which I can see the name of the files and the number of them.
 # The code here is my first attempt.
 
 # The following code direct us to data set which will be used in this project.
-path = "/Users/hinmingfrankiechik/Desktop/text_files"
+corpus_root = "/Users/hinmingfrankiechik/Desktop/text_files"
 
 # I am trying to store all the file names in this list
 filelist=[]
-ignore_files={'.DS_Store', '.txt', 'desktop.ini', 'Huovinen.dataset'}
+ignored_files={'.DS_Store', 'desktop.ini', 'Huovinen.dataset'}
 sorted(filelist)
 
-for root, dirs, files in os.walk(path):
+for root, dirs, files in os.walk(corpus_root):
     for file in files:
-        if file not in ignore_files:
-            filelist.append(os.path.join(file))
+        if file not in ignored_files:
+            filelist.append(os.corpus_root.join(file))
 
 # Print all the files in all subdirectories.
 for name in filelist:
@@ -43,8 +52,6 @@ index.name = "Num"
 df[['Author', 'Year', 'Title']] = df.Tent.str.split("_-_", expand=True)
 del df['Tent']
 
-print("\n After adding three new columns and deleting the original column : \n", df)
-
 # Problem remains: I have six txt files which do not have the Authors' names in their title, which make my list looks strange.
 mask = pd.to_numeric(df['Author'], errors='coerce').notnull()
 df[mask] = df[mask].shift(axis=1)
@@ -55,57 +62,59 @@ print (df)
 # Now, we can print the list of files my data set has.
 df.to_csv('/Users/hinmingfrankiechik/Desktop/Bibliography.csv', index = False)
 
-# Before processing Topic Modeling, I will add a stop words list to my data set so that no common words in English will be analyzed in my code.
-# This is a simple stop words list in NLTK.
-stop_words = set(stopwords.words('english'))
-print("Stop Words", list(stop_words)[:30])
+
+stop_words = nltk.corpus.stopwords.words('english')
+print(len(stop_words))
 
 # However, the list originally in NLTK is not enough. I have tried to analyze my data set but some words like 'Author', numbers of years were included.
 # Thus, I have to create an extended stop words list.
-new_sw = [
+stop_words.extend ([
     'JSTOR', 'Author', 'which', 'who', 'hello', 'school', 'from', 'subject', 'edu', 'use',
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
     'pp', 'data', 'al', 'vol', 'Open', 'Access', 'Accepted', 'Received', 'article', 'available' ,'License'
     'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
-    'grateful', 'total', 'member'
-]
-new_stopwords_list = stop_words.union(new_sw)
+    'grateful', 'total', 'member', 'year', 'month', 'Fig', 'primarily', 'compared', 'group', 'potentially', 'based',
+    'ways', 'shown', 'expression'
+])
 print(len(stop_words))
+
+stemmer = nltk.stem.PorterStemmer()
+lemm = WordNetLemmatizer()
 
 texts = []
 labels = []
 
 for path, dirs, files in os.walk('/Users/hinmingfrankiechik/Desktop/text_files'):
     for file in files:
-        if file not in ignore_files:
+        if file not in ignored_files:
             with open(os.path.join(path, file)) as rf:
                 text = rf.read()
-                tokens = nltk.word_tokenize(text)
-                cleaned = [word for word in tokens if word.isalnum()]
-                texts.append(cleaned)
+                lemma_text=lemmatizer.lemmatize(text)
+                tokenized_texts = nltk.word_tokenize(lemma_text)
+                tokenized_texts_cleaned = [word for word in tokenized_texts if word.lower() not in stop_words]
+                tokenized_texts_cleaned_no_punctuation = [word for word in tokenized_texts_cleaned if word.isalnum()]
+                texts.append(tokenized_texts_cleaned_no_punctuation)
                 labels.append(file[:-4])
 
-corpus_dictionary = gensim.corpora.Dictionary(texts)
+id2word = gensim.corpora.Dictionary(texts)
 
-processed_corpus = [corpus_dictionary.doc2bow(text) for text in texts]
-print(processed_corpus[0])
-
-number_of_topics=5
+ready_for_analysis_corpus = [id2word.doc2bow(text) for text in texts]
+print(ready_for_analysis_corpus[0])
 
 mallet_path = '/Users/hinmingfrankiechik/Downloads/mallet-2.0.8/bin/mallet'
 
 lda_model = gensim.models.wrappers.ldamallet.LdaMallet(
 mallet_path,
-    corpus=processed_corpus,
-    id2word=corpus_dictionary,
-    num_topics=number_of_topics,
+    corpus=ready_for_analysis_corpus,
+    id2word=id2word,
+    num_topics=10,
     optimize_interval=10,
-    prefix='txt200_'
+    prefix='txt200x_'
 )
 
-lda_model.save("CDH598_200.p")
+lda_model.save("CDH598_200x.p")
 
-topics = lda_model.show_topics(num_topics=number_of_topics, num_words=20)
+topics = lda_model.show_topics(num_topics=10, num_words=20)
 
 for topic in topics:
     print(f"Topic #{topic[0]}: {topic[1]}...\n\n")
@@ -113,13 +122,18 @@ for topic in topics:
 # Now, let's visualize the topic modelling result.
 # I create a function that allows me to transfer the results of each year into pandas. After that, the results shown in pandads will be visualized in mutiple bar charts.
 
-names = ['Topic', 'Weight', 'Words']
+names = ['Topic', 'Weight', 'KeyWords']
 def read_table (txt_files):
     list(open(txt_files))
     df = pd.read_table(txt_files, names=names)
-    del df['Words']
+    del df['KeyWords']
     print(df)
-    df.plot.bar(x='Topic', y='Weight')
+    # The pandas data frame, which layouts the result of the above analysis, is created.
+    # The following code will create a bar chart in which the pandas data frame can be visualized
+    df.plot.bar(index, weights)
+    plt.xticks(index, labels, rotation=90)
+    plt.xlabel('Topic')
+    plt.ylabel('Weight')
     plt.title("Topic Weights")
     plt.tight_layout()
     plt.show()
@@ -127,3 +141,12 @@ def read_table (txt_files):
 
 df2 = read_table ('/Users/hinmingfrankiechik/Desktop/txt200_topickeys.txt')
 print (df2)
+
+# The above visualization shows us the weights of each topic. We can see the dominant topics across the entire corpus.
+# What are the weights of the topic in an individual document?
+# Which topic(s) are continuously dominant in every document?
+# In this part of my code, I am going to see the topic weights by documents.
+# The following code analyzes the occurrence of the keywords of each topic.
+
+# Let's try to visualize the keywords in wordclouds for each topic.
+
