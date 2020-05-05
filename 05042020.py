ce088d458c4659dfa4a863caa309092a59c4f352
FrankieChik <59936546+FrankieChik@users.noreply.github.com>
2020-05-04 19:05:13 -0700

    Update 05042020.py

--- 05042020.py
+++ 05042020.py
@@ -74,46 +74,29 @@ plt.title("Number of Publications by Years")
 plt.tight_layout()
 plt.show()
 
+# Proprocessing the corpus.
 texts = []
 labels = []
 
 stop_words = nltk.corpus.stopwords.words('english')
 print(len(stop_words))
 
-# However, the list originally in NLTK is not enough. I have tried to analyze my data set but some words like 'Author', numbers of years were included.
-# Thus, I have to create an extended stop words list.
-stop_words.extend ([
-    'jstor', 'author', 'which', 'who', 'hello', 'school',
-    'from', 'subject', 'edu', 'use', 'pp', 'data', 'al', 'vol',
-    'Open', 'access', 'accepted', 'received', 'article', 'available', 'license'
-    'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august',
-    'september', 'october', 'november', 'december', 'grateful', 'total', 'member',
-    'year', 'month', 'fig', 'primarily', 'compared', 'group', 'potentially', 'based',
-    'ways', 'shown', 'expression', 'Table', 'low', 'found', 'study', 'called', 'journal',
-    'two', 'usa', 'et', 'new', 'also', 'table', 'type', 'page', 'see', 'using', 'oral', 'role',
-    'used', 'manuscript', 'figure', 'read'
-])
-print(len(stop_words))
-
 porter_stemmer = PorterStemmer()
 lemmatizer = WordNetLemmatizer()
 
-
 for path, dirs, files in os.walk('/Users/hinmingfrankiechik/Desktop/text_files'):
     dirs.sort()
-    for dir in dirs:
-        print(os.path.join(path, dir))
     for file in files:
         if file not in ignored_files:
-            print(os.path.join(path, dir, file))
             with open(os.path.join(path, file)) as rf:
                 text = rf.read()
                 tokenized_texts = nltk.word_tokenize(text)
                 lowercase_texts = [word.lower() for word in tokenized_texts]
                 len_nonum_tokenized_texts=[word for word in lowercase_texts if len(word) > 2]
                 tokenized_texts_cleaned = [word for word in len_nonum_tokenized_texts if not word in stop_words]
-                lemma_texts = [lemmatizer.lemmatize(word) for word in tokenized_texts_cleaned]
-                tokenized_texts_cleaned_no_punctuation = [word for word in lemma_texts if word.isalnum()]
+                lemma_texts=[lemmatizer.lemmatize(word) for word in tokenized_texts_cleaned]
+                stem_texts=[stemmer.stem(word) for word in lemma_texts]
+                tokenized_texts_cleaned_no_punctuation = [word for word in stem_texts if word.isalnum()]
                 nonum_tokenized_texts=[word for word in tokenized_texts_cleaned_no_punctuation if not word.isnumeric()]
                 texts.append(nonum_tokenized_texts)
                 labels.append(file[:-4])
@@ -126,10 +109,36 @@ trigram_mod = gensim.models.phrases.Phraser(trigram)
 texts = [bigram_mod[word] for word in texts]
 texts = [trigram_mod[bigram_mod[word]] for word in texts]
 
+new_texts=[]
+for i in texts:
+    if isinstance(i, list):
+        new_texts.extend(i)
+    else:
+        new_texts.append(i)
 
-id2word = gensim.corpora.Dictionary(texts)
+fdist = FreqDist(new_texts)
+fdist.most_common(500)
+df = pd.DataFrame(list(fdist.most_common(500)),columns = ['column1','column2'])
+print(df)
+
+stop_words.extend ([
+    'jstor', 'author', 'which', 'who', 'hello', 'school', 'from', 'subject', 'edu', 'use',
+    'pp', 'data', 'al', 'vol', 'Open', 'access', 'accepted', 'received', 'article', 'available',
+    'license', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september',
+    'october', 'november', 'december', 'grateful', 'total', 'member', 'year', 'month', 'fig', 'primarily',
+    'compared', 'group', 'potentially', 'based', 'ways', 'shown', 'expression', 'Table', 'low', 'found',
+    'study', 'called', 'journal', 'two', 'usa', 'et', 'new', 'also', 'table', 'type', 'page', 'see', 'using',
+    'oral', 'role', 'used', 'manuscript', 'figure', 'read', 'one', 'two', 'used',
+    'different', 'http', 'however', 'found', 'well', 'many', 'thus', 'within', 'could',
+    'new', 'known', 'three', 'first', 'either', 'even', 'since', 'given', 'common', 'site', 'involved'
+])
+print(len(stop_words))
+
+new_corpus=[word for word in texts if not word in stop_words]
+
+id2word = gensim.corpora.Dictionary(new_corpus)
 id2word.filter_extremes(no_below=5)
-ready_for_analysis_corpus = [id2word.doc2bow(text) for text in texts]
+ready_for_analysis_corpus = [id2word.doc2bow(text) for text in new_corpus]
 print(ready_for_analysis_corpus[0])
 
 mallet_path = '/Users/hinmingfrankiechik/Downloads/mallet-2.0.8/bin/mallet'
@@ -150,52 +159,45 @@ topics = lda_model.show_topics(num_topics=10, num_words=20, formatted=False)
 for topic in topics:
     print(f"Topic #{topic[0]}: {topic[1]}...\n\n")
 
-# Now, let's visualize the topic modelling result.
-# I create a function that allows me to transfer the results of each year into pandas.
-# After that, the results shown in pandads will be visualized in mutiple bar charts.
+# I am going to visualize the result.
+# We can see the topic weights in each documents and the dominant topic in a certain document.
 
-names = ['Topic', 'Weight', 'Words']
-def read_table (txt_files):
-    list(open(txt_files))
-    df = pd.read_table(txt_files, names=names)
-    del df['Words']
-    print(df)
-    df.plot.bar(index, weights)
-    plt.xticks(index, labels, rotation=90)
-    plt.xlabel('Topic')
-    plt.ylabel('Weight')
-    plt.title("Topic Weights")
-    plt.tight_layout()
-    plt.show()
-    return
+with open('/Users/hinmingfrankiechik/Desktop/txt200_doctopics.txt','r', encoding='utf8') as rf:
+    topic_info = rf.read().split("\n")
+    topic_info = [item for item in topic_info if item != ""]
+print(topic_info)
 
-df2 = read_table ('/Users/hinmingfrankiechik/Desktop/txt200_topickeys.txt')
-print (df2)
+doc_topic_info={}
 
-# The above visualization shows us the weights of each topic. We can see the dominant topics across the entire corpus.
-# What are the weights of the topic in an individual document?
-# Which topic(s) are continuously dominant in every document?
-# In this part of my code, I am going to see the topic weights by documents.
-# The following code analyzes the occurrence of the keywords of each topic.
+for name, line in zip(filelist, topic_info):
+    new_info = line.split("\t")
+    doc_topic_info[name] = [float(weight) for weight in new_info[2:]]
+weights = doc_topic_info
+print(doc_topic_info)
 
 # The weights contain some exponential numbers. So I need to float them in my pandas datafrmae.
 pd.options.display.float_format = '{:.5f}'.format
 df=pd.DataFrame(doc_topic_info)
 # transpose the rows and columns in the df.
 df_transposed=df.T
-df_transposed.columns=['Topic_0', 'Topic_1', 'Topic_2', 'Topic_3', 'Topic_4', 'Topic_5', 'Topic_6', 'Topic_7', 'Topic_8', 'Topic_9']
+df_transposed.columns=['Topic_0', 'Topic_1', 'Topic_2', 'Topic_3',
+                       'Topic_4', 'Topic_5', 'Topic_6', 'Topic_7',
+                       'Topic_8', 'Topic_9'
+                       ]
 # Adding a new column which shows the dominant topic of each document by by counting the maximum value in a row.
 df_transposed['Dominant_Topic'] = np.argmax(df_transposed.values, axis=1)
 df_transposed.head()
 print(df_transposed)
 # Save the result. But the csv.file would not float the exponential number if I do not add "float_format='%.5f'".
-df_transposed.to_csv('/Users/hinmingfrankiechik/Desktop/Topic_Weights_in_Documents.csv', index = True, float_format='%.5f')
-
-# Problems:
-# How to add stopwords? Some words like 'Author' do exist in my stopword list, but they still appear in my topic modelling,
-# I may wrongly install stemmer and lemmatizer.
-# How to count the weights of each keyword in a topic?
-# Some files are empty.
+df_transposed.to_csv('/Users/hinmingfrankiechik/Desktop/Topic_Weights_in_Documents.csv',
+                     index = True, float_format='%.5f'
+                     )
+sorted_df = df_transposed['Dominant_Topic'].value_counts().sort_index(ascending=True)
+print(sorted_df)
+sorted_df.plot.bar(x='Topic', y='Dominant_in_Documents')
+plt.title("Dominant Topics in Documents")
+plt.tight_layout()
+plt.show()
 
 data_flat = [w for word_list in texts for w in word_list]
 counter = Counter(data_flat)
